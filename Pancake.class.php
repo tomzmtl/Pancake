fd9a528eb3f5f3309098a319c5a3ef1726339dce
tomzmtl <thomas.andreo@gmail.com>
2013-04-02 06:58:50 -0700

    Initial commit

---
+++ Pancake.class.php
@@ -0,0 +1,166 @@
+<?php
+
+/*
+
+  PANCAKE
+  --------------------
+
+  A PDO-based MySQL Abstraction class.
+  Inspired by the simplicity of EzSQL & Wordpress' WPDB.
+
+*/
+
+
+Class Pancake
+{
+  private $db_name = NULL;
+  private $db_user = NULL;
+  private $db_pass = NULL;
+  private $db_host = NULL;
+
+  private $session = NULL;
+
+  function __construct( $name, $user, $pass, $host = "localhost" )
+  {
+    $this->db_name = $name;
+    $this->db_user = $user;
+    $this->db_pass = $pass;
+    $this->db_host = $host;
+
+    $this->session = "mysql:host=$this->db_host;dbname=$this->db_name";
+  }
+
+  /**
+    * Creates a new PDO session.
+    *
+    * @return object
+    * The PHP PDO object created.
+    *
+  */
+  private function newSession()
+  {
+    return new PDO( $this->session, $this->db_user, $this->db_pass);
+  }
+
+
+  /**
+    * Converts an array of data to process into a SQL-readable
+    * string. ex: "(key1,ke2,key3)"
+    *
+    * @param  array
+    * @return string
+    *
+  */
+  private function getKeys( $data )
+  {
+    return implode( "," , array_keys( $data ));
+  }
+
+
+  /**
+    * Converts an array of data to process into a PDO-compatible
+    * placeholders string. ex: "(?,?,?)"
+    *
+    * @param  array
+    * @return string
+    *
+  */
+  private function getPlaceholders( $data )
+  {
+    $placeholders = array();
+
+    foreach ( $data as $d )
+    {
+      $placeholders[] = "?";
+    }
+
+    return $placeholders = implode( "," , $placeholders );
+  }
+
+
+  /**
+    * Converts a WHERE-like array into a SQL-readable
+    * coondition. ex: "a = 1 AND b = 'b'"
+    *
+    * @param  array
+    * @return string
+    *
+  */
+  private function getConditions( $where )
+  {
+    $conditions = array();
+
+    foreach ( $where as $field => $value )
+    {
+      if ( is_int( $value ) )
+        $conditions[] = "$field = $value";
+      else
+        $conditions[] = "$field = '$value'";
+    }
+
+    return implode( " AND " , $conditions );
+  }
+
+
+  /**
+    * Insert a new entry in the DB. Takes as argument a data array
+    * with key/value pairs. Keys names must match the table's
+    * column names.
+    *
+    * @param string $table
+    * Table to process the insertion into.
+    *
+    * @param array $data
+    * Set of key/value pairs. Keys must match the table's columns names.
+    *
+    * @return mixed
+    * Insert ID (int) if insert is successful, or FALSE on failure.
+    *
+  */
+  public function insert( $table, $data )
+  {
+    $keys = $this->getKeys($data);
+    $placeholders = $this->getPlaceholders($data);
+
+    $q = "INSERT INTO $table ($keys) VALUES ($placeholders)";
+
+    error_log($q);
+
+    $dbh = $this->newSession();
+
+    $stmt = $dbh->prepare($q);
+    $stmt->execute( array_values($data) );
+  }
+
+
+  /**
+    * Deletes one or multiple entries based on a set of conditions.
+    *
+    * @param string $table
+    * Table to delete the entry from.
+    *
+    * @param array $where
+    * An array-based single condition to target the item(s) to delete.
+    * Exemple : array( 'id' => 23 ) equals to the query WHERE id = 234.
+    *
+    * @return mixed
+    * Number of deleted entries (int) if the query didn't fail.
+    * FALSE if a problem occured.
+    *
+    * Note : this method can return 0 (int), so be careful when
+    * processing the output.
+    *
+  */
+  public function delete( $table, $where )
+  {
+    $conditions = $this->getConditions( $where );
+
+    $q = "DELETE FROM $table WHERE $conditions";
+
+    $dbh = $this->newSession();
+
+    $stmt = $dbh->prepare($q);
+
+    $stmt->execute();
+  }
+}
